f384771a33fd2e56b4e6d3ffaa29ce75f902c736
Charles Bin <charles.bin@gmail.com>
2017-08-25 15:25:03 +0700

    Layout and border changes for online users panel

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,20 +1,26 @@
 import React from 'react'
 import { Badge, Glyphicon, ListGroup, ListGroupItem } from 'react-bootstrap'
 
-const listStyle = {
+const listItemStyle = {
   'borderStyle': 'none',
   'marginBottom': '1px',
+  'paddingLeft': '20px',
 }
 
 const groupStyle = {
   'height': '88%',
   'overflow': 'auto',
   'overflowWrap': 'break-word',
-  'marginTop': '15%',
 }
 
 const divStyle = {
   'height': '100%',
+}
+
+const headerStyle = {
+  'whiteSpace': 'nowrap',
+  'borderBottom': '1px solid #ddd',
+  'padding': '0 8px 13px 8px',
 }
 
 export default function Users(props) {
@@ -26,14 +32,14 @@ export default function Users(props) {
   )
   return (
     <div style={divStyle}>
-      <h4 style={{'whiteSpace': 'nowrap'}}>Online Users <Badge>{userList.length}</Badge></h4>
+      <h4 style={headerStyle}>Online Users <Badge>{userList.length}</Badge></h4>
       <ListGroup style={groupStyle}>
         { orderedUserList.map((user, i) => {
           const handleItemClick = createToggleChatHandler(user)
           return (
             <ListGroupItem
               key={i}
-              style={ i > 0 ? listStyle : Object.assign({}, listStyle, {'paddingTop': 0}) }
+              style={ i === 0 ? {...listItemStyle, 'marginTop': '1px'} : listItemStyle}
               onClick={ username !== user ? handleItemClick : null }>
               { user}
               { chatTabs.indexOf(user) !== -1 && <span> <Glyphicon glyph="comment" /></span> }
